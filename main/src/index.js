0e0f49302e5a84f511791f9bf4c8e7ecea146a59
N1ckorish <n1ckorish14@gmail.com>
2021-10-30 22:54:26 +1100

    Team Cards generate dynamically in generateHTML.js and then to generated.html

--- main/src/index.js
+++ main/src/index.js
@@ -13,20 +13,20 @@ const Intern = require("../lib/intern");
 
 
 // Team Member empty arrays
-let employeeInfo = [];
-let engineerInfo = [];
-let managerInfo = [];
-let internInfo = [];
+let employeeArray = [];
+let engineerArray = [];
+let managerArray = [];
+let internArray = [];
 
 
 function teamMemberBuild(){
-    console.log(employeeInfo)
-    console.log(engineerInfo)
-    console.log(managerInfo)
-    console.log(internInfo)
+    console.log(employeeArray)
+    console.log(engineerArray)
+    console.log(managerArray)
+    console.log(internArray)
 
-    let html = generateHTML(employeeInfo, engineerInfo, managerInfo, internInfo)
-    fs.writeFile("./main/src/generateHTML.js", html, (err) => {
+    let html = generateHTML(employeeArray, engineerArray, managerArray, internArray)
+    fs.writeFile("../output/generated.html", html, (err) => {
         if (err)
             console.log(err);
         else {
@@ -38,6 +38,12 @@ function teamMemberBuild(){
 
 // INTIAL MANAGER PROMPTS >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
 const managerpromts = [
+    {
+        type: "input",
+        message: "Managerial Title:",
+        name: "managerTitle",
+        validate: (value)=>{if (value){return true} else {return "Please enter a value to continue"}},
+        },
     { 
         type: "input",
         message: "Manager Name:",
@@ -208,7 +214,7 @@ function addManager() {
             }, 
     ]).then((managerAns) => {
         let manager = new Manager (managerAns.name, managerAns.id, managerAns.email, managerAns.officeNumber);
-        managerInfo.push(manager);
+        managerArray.push(manager);
         appChoices();
     });
 }
@@ -265,115 +271,10 @@ function init() {
     inquirer.prompt(managerpromts).then((managerAns) => {
 
         let manager = new Manager (managerAns.name, managerAns.id, managerAns.email, managerAns.officeNumber);
-        managerInfo.push(manager);
+        managerArray.push(manager);
         appChoices();
     });
 }
 
-function generateHTML(teamArray) {
-    //iterate through employees and send to appropriate function
-    //get the card for each employee type 
-    //once all employees are dealt with call finalHTML
-    let html = "";
-    html +=
-    `
-    <section id="Managers">
-        <div class="container my-5 py-5">
-            <div class="row mb-5">
-                <div class="col-12">
-                    <h1 class="fw-bold text-center">${Manager.role}</h1>
-                    <hr>
-                </div>
-                    <div class="row">
-                    ${managerCard}
-                    </div>
-                </div>
-        
-    </section>
-    `
-
-} 
-
-// MANAGER CARD GENERATE 
-function managerCard(manager){
-    //html for manager (returns a string of a bootstrap card html for the manager)
-
-    let html = "";
-    html +=
-    `<div class="col-sm-12 col-md-6 col-lg-3">
-        <div class="card p-4" >
-            <img src="../../assets/images/manager.png" class="card-img-top" alt="...">
-            <div class="card-body text-center">
-                <h2>${answer.addManager.managerTitle}</h2>
-                <h5>Name:${answer.addManager.name}</h5>
-                <h5>Id:${answer.addManager.id}</h5>
-                <h5>Email:${answer.addManager.email}</h5>
-                <h5>Office PH:${answer.addManager.officeNumber}</h5>
-            </div>
-        </div>
-    </div>`
-    
-}
-
-// ENGINEER CARD GENERATE
-function engineerCard(engineer){
-    //html for manager (returns a string of a bootstrap card html for the manager)
-    let html = "";
-    html +=
-    `<div class="col-sm-12 col-md-6 col-lg-3">
-        <div class="card p-4" >
-            <img src="../../assets/images/manager.png" class="card-img-top" alt="...">
-            <div class="card-body text-center">
-                <h2>${answer.addEngineer.engineerTitle}</h2>
-                <h5>Name:${answer.addEngineer.name}</h5>
-                <h5>Id:${answer.addEngineer.id}</h5>
-                <h5>Email:${answer.addEngineer.email}</h5>
-            </div>
-        </div>
-    </div>`
-}
-
-// EMPLOYEE CARD GENERATE
-function employeeCard(employee){
-    let html = "";
-    html +=
-    `<div class="col-sm-12 col-md-6 col-lg-3">
-        <div class="card p-4" >
-            <img src="../../assets/images/manager.png" class="card-img-top" alt="...">
-            <div class="card-body text-center">
-                <h2>${answer.addEmployee.employeeSection}</h2>
-                <h5>Name:${answer.addEmployee.name}</h5>
-                <h5>Id:${answer.addEmployee.id}</h5>
-                <h5>Email:${answer.addEmployee.email}</h5>
-                <h5>Office PH:${answer.addEmployee.github}</h5>
-            </div>
-        </div>
-    </div>`
-    //html for manager (returns a string of a bootstrap card html for the manager)
-}
-
-// INTERN CARD GENERATE
-function internCard(intern){
-    let html = "";
-    html +=
-    `<div class="col-sm-12 col-md-6 col-lg-3">
-        <div class="card p-4" >
-            <img src="../../assets/images/manager.png" class="card-img-top" alt="...">
-            <div class="card-body text-center">
-                <h2>${answer.addIntern.internSection} Intern</h2>
-                <h5>Name:${answer.addIntern.name}</h5>
-                <h5>Id:${answer.addIntern.id}</h5>
-                <h5>Email:${answer.addIntern.email}</h5>
-                <h5>Office PH:${answer.addIntern.github}</h5>
-            </div>
-        </div>
-    </div>`
-    //html for manager (returns a string of a bootstrap card html for the manager)
-
-}
-
-
-   
-  
 init();
 
